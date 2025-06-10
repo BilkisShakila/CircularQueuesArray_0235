057bb70638ca463d7d8e5e00a3341efee0dc92cc
BilkisShakila <bilkisshakilaa@gmail.com>
2025-06-10 11:48:31 +0700

    make a class queues

--- Circularqueues.cpp
+++ Circularqueues.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class Queues
+{
+private:
+    int FRONT, REAR, max = 5;
+    int queue_array[5];
+
+public:
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+    
